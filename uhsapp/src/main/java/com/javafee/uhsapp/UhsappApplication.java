c4d9cfbe40274cca2c1e1e6668fce4285c470103
enyun3 <cytrusss.b@gmail.com>
2022-04-30 15:39:42 +0200

    manage oauth2 with local user database - oidc user service provide a custom OidcUserService optimization code

--- uhsapp/src/main/java/com/javafee/uhsapp/UhsappApplication.java
+++ uhsapp/src/main/java/com/javafee/uhsapp/UhsappApplication.java
@@ -2,14 +2,8 @@ package com.javafee.uhsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySources({
-		@PropertySource("classpath:application.properties"),
-		@PropertySource("classpath:auth0.properties")
-})
 public class UhsappApplication {
 
 	public static void main(String[] args) {
--- uhsapp/src/main/java/com/javafee/uhsapp/service/impl/UserDetailsServiceImpl.java
+++ uhsapp/src/main/java/com/javafee/uhsapp/service/impl/UserDetailsServiceImpl.java
@@ -4,7 +4,10 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.security.auth.login.LoginException;
 
+import com.javafee.uhsapp.model.domain.UserData;
+import com.javafee.uhsapp.model.repository.UserDataRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.javafee.uhsapp.service.CustomUserDetailsService;
@@ -15,14 +18,21 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements CustomUserDetailsService {
 
+	private final UserDataRepository userDataRepository;
+
 	@Override
 	public UserDetails loadUserByUsername(String login) {
-		return null;
-		//TODO: handle loadUserByUsername
+		return userDataRepository.findByLogin(login).orElseThrow(() -> new UsernameNotFoundException("User not present"));
 	}
 
 	@Override
-	public void createUser(String login, String password) throws NoSuchAlgorithmException, LoginException {
-		//TODO: handle create user
+	public void createUser(String login) throws NoSuchAlgorithmException, LoginException {
+		System.out.println("lol");
+		if (!userDataRepository.findAllByLogin(login).isEmpty())
+			throw new LoginException("User with given login already exists");
+		userDataRepository.save(
+				UserData.builder()
+						.login(login)
+						.build());
 	}
 }
