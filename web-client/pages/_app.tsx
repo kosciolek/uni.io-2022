6cac99d5088ffc3323aeefc6f160d01bc0a7d9db
kosciolek <45483493+kosciolek@users.noreply.github.com>
2022-05-01 10:45:23 +0200

    set up msw

--- web-client/pages/_app.tsx
+++ web-client/pages/_app.tsx
@@ -4,6 +4,10 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import { CssBaseline } from "@mui/material";
 
+if (process.env.NEXT_PUBLIC_API_MOCKING === "enabled") {
+  require("../mocks");
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(() => new QueryClient());
 
