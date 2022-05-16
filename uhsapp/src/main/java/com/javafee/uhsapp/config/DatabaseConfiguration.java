b72535eff1e9860ac09ae9d130d9c36b69bb6912
the-metapod <novitzkimain@gmail.com>
2022-05-16 14:21:37 +0200

    rename backend project folder

--- uhsapp/src/main/java/com/javafee/uhsapp/config/DatabaseConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.javafee.uhsapp.config;
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EntityScan(basePackages = "com.javafee.uhsapp.model")
-public class DatabaseConfiguration {
-}
