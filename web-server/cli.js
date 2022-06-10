c5ff150087208f068a47277b78ddb9350d6f7c75
kosciolek <45483493+kosciolek@users.noreply.github.com>
2022-06-10 19:16:45 +0200

    cli

---
+++ web-server/cli.js
@@ -0,0 +1,54 @@
+const { PrismaClient } = require('@prisma/client');
+const { program } = require('commander');
+const chalk = require('chalk');
+
+const formatReport = ({ reporterId, reason, post }) =>
+  `  ${chalk.red('Post ID')}: ${post.id}
+  ${chalk.red('Reporter ID')}: ${reporterId}
+  ${chalk.red('Report reason')}: ${reason}
+  ${chalk.green('Title')}: ${post.title}
+  ${chalk.green('Author')}: ${post.authorNickname}
+  ${chalk.green('Short description')}: ${post.shortDescription}
+  ${chalk.green('Phone')}: ${post.phone}
+  ${chalk.green('Email')}: ${post.email}
+  ${chalk.green('Address')}: ${post.address}`;
+
+const main = async () => {
+  const client = new PrismaClient();
+
+  /*   await client.postReport.create({
+    data: {
+      reason: 'oszust',
+      reporterId: 'Asdad',
+      postId: 2,
+    },
+  });
+
+  await client.postReport.create({
+    data: {
+      reason: 'oszust213',
+      reporterId: 'Asdad',
+      postId: 2,
+    },
+  }); */
+
+  program
+    .command('view-reports')
+    .description('Allows to view reports and posts reported by users.')
+    .action(async () => {
+      const reports = await client.postReport.findMany({
+        include: {
+          post: true,
+        },
+      });
+
+      console.log(`${reports.length} post reports:\n`);
+      const formatted = reports.map((report) => formatReport(report));
+      const separator = '\n--------------------\n';
+      console.log(formatted.join(separator));
+    });
+
+  program.parse();
+};
+
+main();
